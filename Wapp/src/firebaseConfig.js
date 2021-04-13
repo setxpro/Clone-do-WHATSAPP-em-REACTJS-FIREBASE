f51e993f9c67b50e50059ca71f023df7a468d4b7
setxpro <69186374+setxpro@users.noreply.github.com>
2021-04-13 14:54:55 -0300

    Add files via upload

---
+++ Wapp/src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyDS-VBAP05ZL-QP-6yBACla5G9FtRlYl3o",
+    authDomain: "whatsappclone-4e80f.firebaseapp.com",
+    databaseURL: "https://whatsappclone-4e80f.firebaseio.com",
+    projectId: "whatsappclone-4e80f",
+    storageBucket: "whatsappclone-4e80f.appspot.com",
+    messagingSenderId: "947257788451",
+    appId: "1:947257788451:web:85b68a569d184d864d4996"
+};
